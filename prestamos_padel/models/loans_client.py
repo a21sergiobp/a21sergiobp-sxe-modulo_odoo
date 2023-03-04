2a694c4c471ca8d64ba9749b26d7f6a0ca905632
Sobremesa <a21sergiobp@iessanclemente.net>
2023-03-04 17:01:18 +0100

    heredado cliente de res.partner

--- prestamos_padel/models/loans_client.py
+++ prestamos_padel/models/loans_client.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
+from lxml import etree
 
 logger = logging.getLogger(__name__)
 
@@ -15,3 +16,13 @@ class LoansCliente(models.Model):
     _description = 'Clientes inscritos'
 
     birthDate = fields.Date("Data nacemento", required=True)
+
+    @api.model
+    def fields_view_get(self, view_id='view_partners_form_crm1', view_type='form', toolbar=False, submenu=False):
+        res = super(LoansCliente, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+        if view_type == 'form':
+            doc = etree.XML(res['arch'])
+            for node in doc.xpath("//field[@name='city'] | //field[@name='country_id'] | //field[@name='is_company'] | //field[@name='activity_type_id'] | //field[@name='commercial_partner_id']"):
+                node.set('invisible', '1')
+            res['arch'] = etree.tostring(doc)
+        return res
