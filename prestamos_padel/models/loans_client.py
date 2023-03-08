e04080fee5474ce96e3d9e739a697f9a19456dbb
Sobremesa <a21sergiobp@iessanclemente.net>
2023-03-08 19:19:59 +0100

    engadidos campos por herencia de res.partner email e phone e eliminados permisos non necesarios

--- prestamos_padel/models/loans_client.py
+++ prestamos_padel/models/loans_client.py
@@ -12,27 +12,35 @@ logger = logging.getLogger(__name__)
 class LoansCliente(models.Model):
     _name = 'loans.client'
     _inherits = {'res.partner': 'partner_id'}
-    #_inherit='res.partner'
     _description = 'Clientes inscritos'
 
     partner_id = fields.Many2one('res.partner', string='Partner',  ondelete='cascade')
     #id = fields.Integer(string='ID', readonly=True)
-    name = fields.Char('Nome', required=True)
+    name = fields.Char(related='partner_id.name', string='Nome', required=True, readonly=False)
     dni = fields.Char('DNI', required=True)
     birthDate = fields.Date("Data nacemento", required=True)
-    #email = fields.Char('E-mail')
-    #phone = fields.Char('Teléfono', required=True)
     loans = fields.One2many('loans.loan', 'client_name',string='Prestamos', readonly=True)
+    email = fields.Char(related='partner_id.email', string='Correo electrónico', readonly=False)
+    phone = fields.Char(related='partner_id.phone', string='Teléfono', required=True, readonly=False)
 
-    #crea un id único para cada rexistro
-    #@api.model
-    #def create(self, vals):
-    #    if vals.get('id', 'New') == 'New':
-    #        vals['id'] = self.env['ir.sequence'].next_by_code('loan.sequence') or 'Error'
-    #    return super(LoansCliente, self).create(vals)
-
+    #Cando creamos un rexistro engadimos os valores no modelo res.partner tamén
     @api.model
     def create(self, vals):
-        partner = self.env['res.partner'].create({'name': vals.get('name')})
+        partner_vals = {'name': vals.get('name'), 'email': vals.get('email'), 'phone': vals.get('phone')}
+        partner = self.env['res.partner'].create(partner_vals)
         vals['partner_id'] = partner.id
         return super(LoansCliente, self).create(vals)
+    
+    #se se modifica un rexistro cambiamolo en res.partner tamén
+    def write(self, vals):
+        result = super(LoansCliente, self).write(vals)
+        if 'name' in vals or 'email' in vals or 'phone' in vals:
+            partner_vals = {}
+            if 'name' in vals:
+                partner_vals['name'] = vals['name']
+            if 'email' in vals:
+                partner_vals['email'] = vals['email']
+            if 'phone' in vals:
+                partner_vals['phone'] = vals['phone']
+            self.partner_id.write(partner_vals)
+        return result
